7d5fd60beefe60fe74d1aea3d7ea3c67d2dfa3a2
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-09-06 20:14:09 -0300

    Add: styles in checkout

--- src/views/Checkout.jsx
+++ src/views/Checkout.jsx
@@ -3,6 +3,7 @@ import { Title } from '../components/ui/Title'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '../components/ui/Button'
 import { CartContext } from '../context/CartContext'
+import './Checkout.css'
 
 export function Checkout () {
   const [isPurchase, setPurchase] = useState(false)
@@ -17,7 +18,7 @@ export function Checkout () {
 		<>
 			<Title>Estado de compra</Title>
 			<section className='card checkout'>
-				<p>{isPurchase ? '✔️ Compra finalizada con exito' : '❌ Ocurrio un error con la compra, revisá que el importe no supere el crédito disponible en tu cuenta'}</p>
+				<p className='checkout__p'>{isPurchase ? '✔️ La compra se realizó con éxito' : '❌ Ocurrio un error con la compra, revisá que el importe no supere el crédito disponible en tu cuenta'}</p>
 				<Button onClick={() => navigate('/')}>
 					Volver al catálogo
 				</Button>
